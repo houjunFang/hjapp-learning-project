545a8093873b2c4b16136be9eea4e693ee3896b4
fanghoujun <fanghoujun@126.com>
2019-11-30 16:51:08 +0800

    项目初始化

---
+++ hjapp-main/src/main/java/com/hjapp/hjappmain/HjappMainApplication.java
@@ -0,0 +1,13 @@
+package com.hjapp.hjappmain;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HjappMainApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HjappMainApplication.class, args);
+    }
+
+}
